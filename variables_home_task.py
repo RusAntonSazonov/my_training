4ab050a1b34a4befca8ab37699d88d5d65f8427e
RusAntonSazonov <115030299+RusAntonSazonov@users.noreply.github.com>
2024-11-17 10:17:32 +0300

    Add files via upload

---
+++ variables_home_task.py
@@ -0,0 +1,7 @@
+#
+
+quantity = 12
+hr = 1.5 # hr - сокращенно hour - час
+course_name = 'Python'
+average_time = hr / quantity
+print("Курс:", course_name + ',', 'всего задач:', str(quantity) + ',', 'затрачено часов', str(hr) + ',', 'среднее вермя выполнения:', str(average_time) + '.')
